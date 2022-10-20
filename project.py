eed3ce0c414463eca575bd7f99d2423e2b016cd9
sqdqdwd <mohaned.menem@gmail.com>
2022-10-20 10:27:49 +0200

    2 commit

--- project.py
+++ project.py
@@ -2,3 +2,6 @@ Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print ("hi")
 hi
+>>> print("welcome")
+welcome
+>>> x=5
